4d200cd1827cf32d5fdd507a94ac099b14c9bedc
George Stagg <george.stagg@posit.co>
2024-05-07 14:33:13 +0100

    Fix tagging of Logical and SpecialConstant

--- src/highlight.js
+++ src/highlight.js
@@ -2,14 +2,13 @@ import { styleTags, tags as t } from "@lezer/highlight";
 
 export const rHighlight = styleTags({
   "repeat while for if else return break next in": t.controlKeyword,
-  "True False": t.bool,
+  "Logical!": t.bool,
   function: t.definitionKeyword,
-  Null: t.null,
   "FunctionCall/VariableName": t.function(t.variableName),
   "FunctionDeclaration/VariableName": t.function(t.definition(t.variableName)),
   Comment: t.lineComment,
   "Numeric Integer Complex Inf": t.number,
-  "Na Null NaN": t.literal,
+  "SpecialConstant!": t.literal,
   String: t.string,
   ArithOp: t.arithmeticOperator,
   BitOp: t.bitwiseOperator,
